c4d442fa62258297e8b3a2c0faeae5d341db995f
Ariel V Lima <arielflima@gmail.com>
2019-12-05 15:26:07 -0300

    adding jsconfig.json for compatibility with react-app-wired

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
-
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
