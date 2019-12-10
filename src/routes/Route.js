953efd818fa6319c63e4ee6fcac5e9b7d3d7bcfb
Ariel V Lima <arielflima@gmail.com>
2019-12-10 18:39:42 -0300

    persisting authentication with redux

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
