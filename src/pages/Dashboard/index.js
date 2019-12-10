f82e856937894ce992df94b6a4971f3ebe91fdd2
Ariel V Lima <arielflima@gmail.com>
2019-12-10 19:38:53 -0300

    created authenticated requisitions with sagas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import api from '~/services/api';
 // import { Container } from './styles';
+
+api.get('appointments');
 
 export default function Dashbord() {
   return <h1>Dashboard</h1>;
